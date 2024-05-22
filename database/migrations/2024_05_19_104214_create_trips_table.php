adcf8ad4d7d397ba808b82ce3faec00a7c43a98a
Maria Pankova <qitelopdo@gmail.com>
2024-05-22 19:45:44 +0300

    migrations: db structure OK

--- database/migrations/2024_05_19_104214_create_trips_table.php
+++ database/migrations/2024_05_19_104214_create_trips_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('trips', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+
             $table->string('title');
             $table->string('slug');
             $table->date('date_to');
