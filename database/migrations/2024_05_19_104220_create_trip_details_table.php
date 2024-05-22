e3ccaef1f444fb48efa34fdde8e702f0288d9e50
Maria Pankova <qitelopdo@gmail.com>
2024-05-22 18:56:33 +0300

    migrations: db structure OK

---
+++ database/migrations/2024_05_19_104220_create_trip_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trip_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('trip_id');
+            $table->foreign('trip_id')->references('id')->on('trips')->cascadeOnDelete();
+            $table->date('date_to');
+            $table->date('date_from');
+
+            $table->unsignedBigInteger('country_id'); // api
+            $table->unsignedBigInteger('city_id'); // api
+
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trip_details');
+    }
+};
---
+++ database/migrations/2024_05_20_152910_create_trip_detail_expenses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trip_expenses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('trip_detail_id');
+            $table->foreign('trip_detail_id')->references('id')->on('trip_details')->cascadeOnDelete();
+
+            $table->unsignedMediumInteger('currency_id'); // api
+            $table->decimal('current_currency_exchange', 8, 2);
+            $table->unsignedTinyInteger('source')->default(0); // cash, card or smth else
+            $table->string('title');
+            $table->mediumText('description')->nullable();
+            // $table->boolean('is_complex')->default(0);
+            
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->foreign('parent_id')->references('id')->on('trip_expenses')->cascadeOnDelete();
+            
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trip_expenses');
+    }
+};
