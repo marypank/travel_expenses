48cd1898c9b410887da7daba57d2e4d40b5aee1e
Maria Pankova <qitelopdo@gmail.com>
2024-05-26 11:29:25 +0300

    set model repos

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -4,41 +4,34 @@ namespace App\Repositories;
 
 use Illuminate\Database\Eloquent\Model;
 
-class BaseRepository implements DefaultRepositoryInterface
+abstract class BaseRepository implements DefaultRepositoryInterface
 {
     /* public function all(array $params = []): Collection
     {
         // todo: Почему тут ошибка, если у Lanuage, например, нет ошибки в этом случае
-        // return BaseModel::all();
+        return $this->model()::all();
     } */
 
-    private $model;
-
-    public function setModel(Model $model): self
-    {
-        $this->model = $model;
-
-        return $this;
-    }
+    protected abstract function model();
 
     public function findById(int|string $id): Model
     {
-        return $this->model::find($id);
+        return $this->model()::find($id);
     }
 
     public function create(array $data): Model
     {
-        return $this->model::create($data);
+        return $this->model()::create($data);
     }
 
     public function delete(int|string $id): void
     {
-        $this->model::where('id', $id)
+        $this->model()::where('id', $id)
             ->delete();
     }
 
     public function update(int|string $id, array $data)
     {
-        return tap($this->model::find($id))->update($data)->fresh();
+        return tap($this->model()::find($id))->update($data)->fresh();
     }
 }
--- app/Repositories/TripRepository.php
+++ app/Repositories/TripRepository.php
@@ -3,9 +3,13 @@
 namespace App\Repositories;
 
 use App\Models\BaseModel;
+use App\Models\Trip;
 use Illuminate\Database\Eloquent\Model;
 
 class TripRepository extends BaseRepository
 {
-    //
+    public function model()
+    {
+        return new Trip();
+    }
 }
