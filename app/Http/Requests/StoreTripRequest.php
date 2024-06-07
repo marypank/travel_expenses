851fcc815265b20c2359adec21cee28762ae47fa
Maria Pankova <qitelopdo@gmail.com>
2024-06-07 20:59:06 +0300

    trip module (add userId to create)

--- app/Http/Requests/StoreTripRequest.php
+++ app/Http/Requests/StoreTripRequest.php
@@ -7,17 +7,11 @@ use Illuminate\Support\Str;
 
 class StoreTripRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Prepare the data for validation.
-     */
     protected function prepareForValidation(): void
     {
         $this->merge([
@@ -25,14 +19,11 @@ class StoreTripRequest extends FormRequest
         ]);
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+    /** @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string> */
     public function rules(): array
     {
         return [
+            'userId' => ['required', 'numeric', 'exists:users,id'],
             'title' => ['required', 'string', 'min:16'],
             'slug' => ['required', 'string', 'min:16'],
             'budget' => ['decimal:2,4'],
