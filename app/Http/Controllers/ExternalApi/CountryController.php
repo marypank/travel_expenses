0cc8a6ba678b4efe681f255aa083024a91210c16
Maria Pankova <qitelopdo@gmail.com>
2024-06-20 21:23:42 +0300

    controllers: external api empty controllers

---
+++ app/Http/Controllers/ExternalApi/CountryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\ExternalApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    public function index(Request $request)
+    {
+        // ::all()
+    }
+
+    public function show(Request $request)
+    {
+        // byId
+    }
+}
---
+++ app/Http/Controllers/ExternalApi/CurrencyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\ExternalApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CurrencyController extends Controller
+{
+    public function index(Request $request)
+    {
+        //
+    }
+
+    public function show(Request $request)
+    {
+        //
+    }
+}
