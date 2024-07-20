20a45eb542e45d79ee280f90b90ab579d3769464
Maria Pankova <qitelopdo@gmail.com>
2024-07-20 17:59:03 +0300

    module: remove unness methods

--- app/Http/Services/BaseService.php
+++ app/Http/Services/BaseService.php
@@ -4,7 +4,6 @@ namespace App\Http\Services;
 
 use App\Models\Dto\BaseDtoInterface;
 use App\Repositories\BaseRepository;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
