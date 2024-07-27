2e3a886ebdf6da640824631e7878bc5a3769e74e
Maria Pankova <qitelopdo@gmail.com>
2024-07-27 20:14:04 +0300

    swagger: init

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,15 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="Travel expenses",
+ *     version="1.0",
+ * ),
+ *  @OA\PathItem(
+ *      path="/api"
+ *  ),
+ */
 abstract class Controller
 {
     //
