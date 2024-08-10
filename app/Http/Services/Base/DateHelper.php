3f53c065435c2e35b5ebbb49472705720748e434
Maria Pankova <qitelopdo@gmail.com>
2024-08-10 19:24:13 +0300

    modules: remake

--- app/Http/Services/Base/DateHelper.php
+++ app/Http/Services/Base/DateHelper.php
@@ -6,6 +6,8 @@ use Carbon\Carbon;
 
 class DateHelper
 {
+    // todo: другая директория
+    // todo: кривое название метода
     public static function checkDateParentMismatch(Carbon $date, Carbon $parentDate): bool
     {
         return $date > $parentDate || $date < $parentDate;
