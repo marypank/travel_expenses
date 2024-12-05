e6a011d4dc39673464ec386747cd3fa54d8cd4a3
Maria Pankova <qitelopdo@gmail.com>
2024-12-05 20:56:45 +0300

    add some todo

--- app/Http/Controllers/ExternalApi/CityController.php
+++ app/Http/Controllers/ExternalApi/CityController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
+    // todo: сделать выбор, тянуть данные из апишки или, если существует, базы данных, т.е. нужен переключатель сервис, интерфейс типа driver
     public function index(Request $request)
     {
         //
