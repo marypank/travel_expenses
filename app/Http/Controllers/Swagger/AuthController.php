d3ccbd06059fc3a4b96e4937453c81a0fc6203c4
Maria Pankova <qitelopdo@gmail.com>
2024-12-05 20:22:07 +0300

    swagger

---
+++ app/Http/Controllers/Swagger/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+class AuthController extends Controller
+{
+    public function login()
+    {}
+
+    public function register()
+    {}
+
+    public function logout()
+    {}
+}
--- app/Http/Controllers/Swagger/TripController.php
+++ app/Http/Controllers/Swagger/TripController.php
@@ -7,6 +7,7 @@ class TripController extends Controller
     /**
      * @OA\Get(
      *     path="/trips",
+     *     description="Get all user trips",
      *     tags={"Trips"},
      *     summary="Get all user trips",
      *     @OA\Response(
@@ -52,6 +53,34 @@ class TripController extends Controller
     public function update()
     {}
 
+    // todo: исправить проблему, что вместо json присылается html. проблема не в swagger, а, возможно, где-то в настройках
+    /**
+     * @OA\Delete(
+     *     path="/trips/{id}",
+     *     tags={"Trips"},
+     *     summary="Delete user trip",
+     *     description="Delete user trip by id",
+     *     operationId="deleteTrip",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Trip id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request",
+     *         @OA\JsonContent(),
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Trip not found",
+     *     )
+     * )
+     */
     public function destroy()
     {}
 
