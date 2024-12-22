a83c14e6ded8d6123d2fb87d2979a19f56517a24
Maria Pankova <qitelopdo@gmail.com>
2024-12-22 12:58:35 +0300

    controllers: new Report controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\Reports\TripReportService;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    public function Trip(int $id)
+    public function __construct(private TripReportService $tripReportService)
     {
-        // expenses by trip by trip id
+        //
+    }
+
+    // todo: вместо id можно загрузить Trip $trip
+    public function trip(int $id)
+    {
+        $result = $this->tripReportService->getSpreadsheet($id);
+        // expenses by trip by trip id (POLICY)
         // todo: check if THIS tripId BELONGS to THIS user
         // xlsx|xls report
     }
---
+++ app/Http/Services/Reports/TripReportService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Services\Reports;
+
+class TripReportService
+{
+    // todo: rename
+    public function getSpreadsheet()
+    {
+        // 1. достать трип и его relations
+        // 2. инициализировать спредшит
+        // 3. записать все в спредшит
+        // 4. выдать, что формирование окончено
+        // 5. позволить скачать
+    }
+}
