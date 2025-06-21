5af3853ede2930e2f714d0bdd6ec24cacf87d6a6
Maria Pankova <qitelopdo@gmail.com>
2025-06-21 18:22:12 +0300

    models

---
+++ app/Models/Trip.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Enum\TripStatusEnum;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Trip extends Model
+{
+    /** @var string $table */
+    protected $table = 'trips';
+
+    /** @var array $fillable */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'date_to',
+        'date_from',
+        'status',
+        'description',
+    ];
+
+    /** @var array $casts */
+    protected $casts = [
+        'date_to' => 'date',
+        'date_from' => 'date',
+        'status' => TripStatusEnum::class,
+    ];
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(TripExpense::class);
+    }
+}
