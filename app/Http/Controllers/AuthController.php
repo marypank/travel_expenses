659be3b2bb7534fe4575c24eae030e3081a79ac2
Maria Pankova <qitelopdo@gmail.com>
2025-06-24 19:49:43 +0300

    add token based auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
-    //
+    // todo: create service for handling
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+        if (!Auth::guard('web')->attempt(['name' => $data['name'], 'password' => $data['password']])) {
+            return response([
+                'data' => '',
+                'message' => 'Login or password incorrect.'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => new UserResource($user),
+            'token' => $token
+        ]);
+
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        /** @var User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => new UserResource($user),
+            'token' => $token
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        /** @var User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response('')->setStatusCode(204);
+    }
 }
