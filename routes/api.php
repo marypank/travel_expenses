467d6e74f13af67926046f50ff8f4da6e10d3762
Maria Pankova <qitelopdo@gmail.com>
2025-06-24 19:57:58 +0300

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CurrencyController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TripController;
+use App\Http\Controllers\TripExpenseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +14,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user',  function (Request $request) {
         return $request->user();
     });
+
+    Route::apiResource('/trips', TripController::class);
+    Route::apiResource('/trip-expenses', TripExpenseController::class);
+    Route::apiResource('/tags', TagController::class);
+    Route::apiResource('/currencies', CurrencyController::class);
 });
 
 Route::post('login', [AuthController::class, 'login']);
