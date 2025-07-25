93e4b8b5d95cd8faeba053a9228beb5bdc6d4f76
Maria Pankova <qitelopdo@gmail.com>
2025-07-25 19:12:16 +0300

    swagger Trip - not finished

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -16,19 +16,20 @@ class TripController extends Controller
     public function __construct(private readonly TripService $tripService)
     {}
 
+    // todo: изменить status и source не на name, а на обычное число
     /**
      * @OA\Get(
      *     path="/api/trips",
      *     description="Get all user trips",
      *     tags={"Trips"},
      *     security={{"bearer_token":{}}},
-     *     summary="Get all currencies",
+     *     summary="Get all trips",
      *     @OA\Response(
      *         response=200,
      *         description="OK",
      *         @OA\JsonContent(
      *             type="array",
-     *             @OA\Items(ref="#/components/schemas/Trip")
+     *             @OA\Items(ref="#/components/schemas/TripResource")
      *         ),
      *     ),
      *     @OA\Response(
@@ -51,6 +52,38 @@ class TripController extends Controller
         return TripResource::collection($this->tripService->all());
     }
 
+    /**
+     * // todo: исключить из RequestBody Trip id, slug
+     * @OA\Post(
+     *     path="/api/trips",
+     *     description="Create trip",
+     *     tags={"Trips"},
+     *     security={{"bearer_token":{}}},
+     *     summary="Create trip",
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid ID supplied"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Trip not found"
+     *     ),
+     *     @OA\Response(
+     *         response=405,
+     *         description="Validation exception"
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Created",
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(ref="#/components/schemas/Trip")
+     *         )
+     *     )
+     * )
+     */
     public function store(StoreTripRequest $request)
     {
         $dto = TripDto::create(...$request->validated());
@@ -68,11 +101,95 @@ class TripController extends Controller
         return response()->noContent(Response::HTTP_CREATED);
     }
 
+    /**
+     * // todo: переделать ответ с массива на один объект (swagger)
+     * @OA\Get(
+     *     path="/api/trips/{id}",
+     *     description="Get single trip by id",
+     *     tags={"Trips"},
+     *     security={{"bearer_token":{}}},
+     *     summary="Get one single trip by id",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="trip id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(ref="#/components/schemas/TripResource"),
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated.",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal Server Error"
+     *      )
+     *    )
+     * )
+     * 
+     * @param Trip $trip
+     * @return TripResource
+     */
     public function show(Trip $trip)
     {
         return new TripResource($trip);
     }
 
+    /**
+     * // todo: исключить из RequestBody Trip id
+     * // todo: переделать ответ с массива на один объект (swagger)
+     * @OA\Patch(
+     *     path="/api/trips/{id}",
+     *     description="Update trip by id",
+     *     tags={"Trips"},
+     *     security={{"bearer_token":{}}},
+     *     summary="Update trip by id",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Trip id to delete",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid ID supplied"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Trip not found"
+     *     ),
+     *     @OA\Response(
+     *         response=405,
+     *         description="Validation exception"
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(ref="#/components/schemas/Trip")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(ref="#/components/schemas/TripResource"),
+     *     ),
+     * )
+     */
     public function update(UpdateTripRequest $request, Trip $trip)
     {
         $dto = UpdateTripDto::create($trip->id, ...$request->validated());
@@ -82,6 +199,36 @@ class TripController extends Controller
         return new TripResource($trip);
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/trips/{id}",
+     *     tags={"Trips"},
+     *     security={{"bearer_token":{}}},
+     *     summary="Deletes a trip",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Trip id to delete",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid ID supplied",
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Trip not found",
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Deleted",
+     *     ),
+     * )
+     */
     public function destroy(Trip $trip)
     {
         $this->tripService->delete($trip->id);
