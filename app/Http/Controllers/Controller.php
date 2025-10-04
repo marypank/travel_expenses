3296105c3225e3c09c261cf5918921af932c2e68
Maria Pankova <qitelopdo@gmail.com>
2025-10-04 11:58:12 +0300

    swagger init

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as RoutingController;
 
+/**
+ * @OA\Info(
+ *     title="Travel expenses",
+ *     version="1.0",
+ * ),
+ * @OA\PathItem(
+ *     path="/api"
+ * ),
+ * @OA\Components(
+ *     @OA\SecurityScheme(
+ *         securityScheme="bearer_token",
+ *         type="http",
+ *         scheme="bearer"
+ *     )
+ * )
+ */
 abstract class Controller extends RoutingController
 {
     use AuthorizesRequests;
